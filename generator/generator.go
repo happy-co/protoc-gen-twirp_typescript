509f854b8b37e92931eb18a61f2c9d35a01a189c
Michael Terrington <michael.terrington@happy.co>
2018-05-29 11:31:49 +0930

    Fix generated file paths to retain proto path

--- generator/generator.go
+++ generator/generator.go
@@ -10,8 +10,7 @@ func tsModuleFilename(f *descriptor.FileDescriptorProto) string {
 	name := *f.Name
 
 	if ext := path.Ext(name); ext == ".proto" || ext == ".protodevel" {
-		base := path.Base(name)
-		name = base[:len(base)-len(path.Ext(base))]
+		name = name[:len(name)-len(path.Ext(name))]
 	}
 
 	name += ".ts"
